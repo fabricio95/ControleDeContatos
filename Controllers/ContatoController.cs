920eb4d5e71f4fb18243eaae71b1f479594f4aa6
fabricio95 <paulofsm95@gmail.com>
2023-04-13 17:18:03 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index() => View();
+        
+        
+    }
+}
