840cb06304689a9550c88c3814c75bb86c2aa11b
fabricio95 <paulofsm95@gmail.com>
2023-05-02 17:17:44 -0300

    Criação da funcionalidade Usuário, e alterações de layout!

---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,27 @@
+﻿using ControleDeContatos.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = " Digite o nome do Usuário")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = " Digite o Login do Usuário")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Digite o Email do Usuário")]
+        [EmailAddress(ErrorMessage = "O Email informado não é válido!")]
+        public string Email { get; set; }
+        public PerfilEnum Perfil { get; set; }
+        [Required(ErrorMessage = " Digite a senha do Usuário")]
+        public string Senha { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime? DataAtualizacao { get; set; }
+
+
+
+    }
+
+}
---
+++ Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,74 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ControleDeContatos.Repositorio
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly BancoContext _context;
+        
+
+        public UsuarioRepositorio(BancoContext bancoContext)
+        {
+            this._context = bancoContext;
+        }
+
+        public UsuarioModel ListarPorId(int id)
+        {
+            return _context.Usuarios.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<UsuarioModel> BuscarTodos()
+        {
+            return _context.Usuarios.ToList();
+        }
+
+        public UsuarioModel Adicionar(UsuarioModel usuario)
+        {
+            usuario.DataCadastro = DateTime.Now;
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+            return usuario;
+        }
+
+        public UsuarioModel Atualizar(UsuarioModel usuario)
+        {
+            UsuarioModel usuarioDB = ListarPorId(usuario.Id);
+
+            if (usuarioDB == null) throw new Exception("Houve um erro na atualização do usuário!");
+
+            usuarioDB.Nome = usuario.Nome;
+            usuarioDB.Email = usuario.Email;
+            usuarioDB.Login = usuario.Login;
+            usuarioDB.Perfil = usuario.Perfil;
+            usuarioDB.DataAtualizacao = DateTime.Now;
+
+            _context.Usuarios.Update(usuarioDB);
+            _context.SaveChanges();
+
+            return usuarioDB;
+            
+        }
+
+        public bool Apagar(int id)
+        {
+            UsuarioModel usuarioDB = ListarPorId(id);
+
+            if (usuarioDB == null) throw new Exception("Houve um erro na exclusão do usuário!");
+
+            _context.Usuarios.Remove(usuarioDB);
+            _context.SaveChanges();
+
+            return true;
+
+        }
+
+        public void Adicionar(object usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
